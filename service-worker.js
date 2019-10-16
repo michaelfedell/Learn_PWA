6931d005cd0a8a98731fe2618d2d2e049c5640a9
Michael Fedell <michaelfedell14@gmail.com>
2019-10-16 10:35:58 -0500

    Add install and activate step to service worker

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,22 @@
 let cacheName = 'weatherPWA';
+let dataCacheName = 'weatherData';
 let filesToCache = [
-
+  'index.html',
 ];
 
 self.addEventListener('install', e => {
+  console.log('Installing');
   e.waitUntil(
     caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('activate', e => {
+  console.log('Activating');
+  e.waitUntil(
+    caches.keys().then(keyList => Promise.all(keyList.map(key => {
+      if (key !== cacheName && key !== dataCacheName)
+        return caches.delete(key);
+    })))
   );
 });
